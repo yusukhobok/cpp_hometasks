838a2e1272bea7d2173650261357841704050604
“Yuri” <yuri.sukhobok@gmail.com>
2023-09-27 11:16:34 +0500

    04: add basic function

--- 04/src/printer.hpp
+++ 04/src/printer.hpp
@@ -1,4 +1,17 @@
 #ifndef PRINTER_HPP
 #define PRINTER_HPP
 
+
+#include <iostream>
+#include <list>
+#include <tuple>
+#include <type_traits>
+#include <vector>
+
+
+void print_ip(const std::string &ip) {
+    std::cout << ip << std::endl;
+}
+
+
 #endif //PRINTER_HPP
