267221ccd875d70ddef26d69c4572e29c7a068f3
“Yuri” <yuri.sukhobok@gmail.com>
2023-09-29 11:27:51 +0500

    04: add basic templates

--- 04/src/printer.hpp
+++ 04/src/printer.hpp
@@ -1,7 +1,6 @@
 #ifndef PRINTER_HPP
 #define PRINTER_HPP
 
-
 #include <iostream>
 #include <list>
 #include <tuple>
@@ -13,5 +12,31 @@ void print_ip(const std::string &ip) {
     std::cout << ip << std::endl;
 }
 
+template<typename T>
+std::enable_if_t<std::is_integral_v<T>, void>
+print_ip(const T &ip) {
+    const auto byte_number = reinterpret_cast<const std::byte *>(&ip);
+    for (auto i = static_cast<int>(sizeof(ip) - 1); i >= 0; --i) {
+        std::cout << static_cast<int>(byte_number[i]);
+        if (i != 0) {
+            std::cout << ".";
+        }
+    }
+    std::cout << std::endl;
+}
+
+template<typename T>
+std::enable_if_t<
+        std::is_same_v<T, std::vector<typename T::value_type>> || std::is_same_v<T, std::list<typename T::value_type>>
+>
+print_ip(const T &ip) {
+    for (const auto &element : ip) {
+        std::cout << element;
+        if (ip.back() != element) {
+            std::cout << ".";
+        }
+    }
+    std::cout << std::endl;
+}
 
 #endif //PRINTER_HPP
