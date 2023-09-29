50b27f7ea7d1004ca0791d41cd0bd680cdd1f703
“Yuri” <yuri.sukhobok@gmail.com>
2023-09-29 21:11:24 +0500

    04: add documentation

--- 04/src/printer.h
+++ 04/src/printer.h
@@ -1,16 +1,31 @@
 #ifndef PRINTER_HPP
 #define PRINTER_HPP
 
+/*!
+	@file printer.h
+	@brief Набор функций для печати условного IP-адреса
+*/
+
 
 #include <list>
 #include <type_traits>
 #include <vector>
 #include <iostream>
 
+
+/*!
+	@brief Тривиальная реализация функции print_ip
+	@param ip Ссылка на строковое представление ip-адреса.
+*/
 void print_ip(const std::string &ip) {
     std::cout << ip << std::endl;
 }
 
+
+/*!
+	@brief Реализация функции print_ip, принимающая целочисленный ip-адрес
+	@param ip Ссылка на целочисленное представление ip-адреса.
+*/
 template<typename T>
 std::enable_if_t<std::is_integral_v<T>, void>
 print_ip(const T &ip) {
@@ -24,6 +39,11 @@ print_ip(const T &ip) {
     std::cout << std::endl;
 }
 
+
+/*!
+	@brief Реализация функции print_ip, принимающая целочисленный ip-адрес
+	@param ip Ссылка на представление ip-адреса в виде контейнера.
+*/
 template<typename T>
 std::enable_if_t<
         std::is_same_v<T, std::vector<typename T::value_type>> || std::is_same_v<T, std::list<typename T::value_type>>
