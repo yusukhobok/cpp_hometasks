771c96c2b868e3e96ece8a093f086b2eb3522b39
“Yuri” <yuri.sukhobok@gmail.com>
2023-10-28 22:58:39 +0500

    7: update

--- 07/timus/1249/main.cpp
+++ 07/timus/1249/main.cpp
@@ -1,58 +1,44 @@
-#define CRT_SECURE_NO_WARNINGS
-
 #include <iostream>
 #include <vector>
-//#include<cstdio>
+#include<cstdio>
+#include <chrono>
 
 // https://acm.timus.ru/problem.aspx?space=1&num=1249
 // 365690IY
 
 using vb = std::vector<bool>;
 
-vb read_line(int m) {
-    vb current_line = vb(m, 0);
-    for (int i = 0; i < m; ++i) {
-        int value;
-//        scanf("%d", &value);
-        std::cin >> value;
-        current_line[i] = value;
-    }
-    return current_line;
-}
-
-bool check(int m, const vb& previous_line, const vb& current_line) {
-    int sum;
-    for (int i = 0; i < m - 1; ++i) {
-        sum = previous_line[i] + previous_line[i+1] + current_line[i] + current_line[i+1];
-        if (sum == 3) {
-            return false;
-        }
-    }
-    return true;
-}
+using milli = std::chrono::milliseconds;
 
 
 int main() {
-    int n, m;
+    auto start = std::chrono::high_resolution_clock::now();
+
+    int n, m, value, sum;
     std::cin >> n >> m;
-    bool should_continue = true;
     bool ok = true;
+    if (n != 0) {
 
-    if (n == 0) {
-        should_continue = false;
-    }
-
-    vb current_line = read_line(m);
-    vb previous_line;
-    for (int i = 1; i < n; ++i) {
-        previous_line = std::move(current_line);
-        current_line = read_line(m);
-        if (!should_continue) {
-            continue;
+        vb current_line = vb(m, 0);
+        for (int i = 0; i < m; ++i) {
+            scanf("%d", &value);
+            current_line[i] = value;
         }
-        if (!check(m, previous_line, current_line)) {
-            should_continue = false;
-            ok = false;
+
+        vb previous_line;
+        for (int i = 1; i < n; ++i) {
+            previous_line = current_line;
+            for (int j = 0; j < m; ++j) {
+                scanf("%d", &value);
+                current_line[j] = value;
+                if (j == 0) {
+                    continue;
+                }
+                sum = previous_line[j] + previous_line[j - 1] + current_line[j] + current_line[j - 1];
+                if (sum == 3) {
+                    ok = false;
+                }
+            }
         }
     }
 
@@ -61,4 +47,10 @@ int main() {
     } else {
         std::cout << "No";
     }
+
+
+    auto finish = std::chrono::high_resolution_clock::now();
+    std::cout << "\ntook "
+              << std::chrono::duration_cast<milli>(finish - start).count()
+              << " milliseconds\n";
 }
