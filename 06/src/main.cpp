f500e830b0d5859ebc1004a5918af3527847ee5a
“Yuri” <yuri.sukhobok@gmail.com>
2023-11-19 23:11:14 +0500

    Add hometask 8

--- 06/src/main.cpp
+++ 06/src/main.cpp
@@ -1,12 +1,28 @@
 #include <list>
 #include <string>
 #include <vector>
+#include <cassert>
 
 #include "matrix.hpp"
 
 
 int main() {
-    Matrix matrix;
-    matrix.hello();
+    Matrix<int, -1> matrix;
+    assert(matrix.size() == 0);
+
+    auto a = matrix[0][0];
+    auto b = static_cast<int>(a);
+    assert (b == -1);
+    assert(matrix.size() == 0);
+
+    matrix[100][100] = 314;
+    assert (matrix[100][100] == 314);
+    assert(matrix.size() == 1);
+
+    for (auto c : matrix) {
+        std::cout << c.first << c.second << c. << std::endl;
+    }
+
+
     return 0;
 }
--- 06/src/matrix.hpp
+++ 06/src/matrix.hpp
@@ -1,16 +1,72 @@
 #ifndef MATRIX_HPP
 #define MATRIX_HPP
 
-
-#include <list>
-#include <type_traits>
-#include <vector>
 #include <iostream>
+#include <map>
 
+using Point = std::pair<int, int>;
+
+template <typename T, T default_value>
 class Matrix {
+private:
+    std::map<Point, T> elements;
+
+    class Row {
+    private:
+        Point coordinates;
+        Matrix* matrix;
+    public:
+        Row(Matrix* matrix, int x) : matrix(matrix) {
+            this->coordinates.first = x;
+            this->coordinates.second = 0;
+        }
+
+        Row& operator[](int y) {
+            coordinates.second = y;
+            return *this;
+        }
+
+        Row& operator=(T value) {
+            this->matrix->add(this->coordinates, value);
+            return *this;
+        }
+
+        operator T() {
+            return this->matrix->get(this->coordinates);
+        }
+    };
+
 public:
-    void hello() {
-        std::cout << "Hello" << std::endl;
+    std::size_t size() {
+        return this->elements.size();
+    }
+
+    auto begin() const {
+        return std::begin(this->elements);
+    }
+
+    auto end() const {
+        return std::end(this->elements);
+    }
+
+    Row operator[](int index) {
+        Row row(this, index);
+        return row;
+    }
+
+    void add(Point point, T value) {
+        if (value != default_value) {
+            this->elements[point] = value;
+        } else if (this->elements.count(point) != 0) {
+            elements.erase(point);
+        }
+    }
+
+    T get(Point point) {
+        if (this->elements.count(point) == 0) {
+            return default_value;
+        }
+        return this->elements[point];
     }
 };
 
