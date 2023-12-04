df31a701f7e1822ac925c82af7ace45e6ff8160e
Yuri Sukhobok <sukhobok.y@numdes.com>
2023-12-04 15:39:33 +0500

    remove all files for review

--- 02/src/ip_address.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include "ip_address.h"
-
-
-std::ostream &operator<<(std::ostream &os, const IpAddress &ip) {
-    os << ip.first << "." << ip.second << "." << ip.third << "." << ip.forth << std::endl;
-    return os;
-}
